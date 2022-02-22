04741879236c2103b1d2f46f73caff0d2ae052ce
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2022-02-22 22:26:03 +0100

    Added level2

--- 2016/day4/solve.erl
+++ 2016/day4/solve.erl
@@ -13,7 +13,7 @@ do_parse_file(FP) ->
 
 parse(S) ->
 	[Checksum, Id | L] = lists:reverse(re:split(S, "[\\[\\]\\-]", [trim, {return, list}])),
-    {lists:flatten(lists:reverse(L)), list_to_integer(Id), Checksum}.
+    {lists:flatten(lists:join("-", lists:reverse(L))), list_to_integer(Id), Checksum}.
 
 % Remove duplicates but preserve order
 remove_dups([]) ->
@@ -22,11 +22,13 @@ remove_dups([H|T]) ->
     [H | [X || X <- remove_dups(T), X /= H]].
 
 checksum({L, _, _}) ->
-    Count = fun(X, {Cnt, M}) ->
-        CntNew = maps:update_with(X, fun(Y) -> Y+1 end, 1, Cnt),
-        {ok, N} = maps:find(X, CntNew),
-        {CntNew, maps:update_with(N, fun(V) -> lists:sort([X|V]) end, [X], M)}
-    end,
+    Count = fun($-, {Cnt, M}) ->
+                {Cnt, M};
+            (X, {Cnt, M}) ->
+                CntNew = maps:update_with(X, fun(Y) -> Y+1 end, 1, Cnt),
+                {ok, N} = maps:find(X, CntNew),
+                {CntNew, maps:update_with(N, fun(V) -> lists:sort([X|V]) end, [X], M)}
+        end,
     {_, Freq} = lists:foldl(Count, {maps:new(), maps:new()}, L),
 
     AccCS = fun(X, Acc) ->
@@ -53,9 +55,25 @@ do1() ->
     end,
     lists:foldl(Sum, 0, L).
 
+decrypt({L, ID, _CS}) ->
+    F = fun($-) ->
+            $ ;
+        (X) ->
+            ((X - $a + ID) rem ($z-$a+1)) + $a
+    end,
+    lists:map(F, L).
+
+# erl -pa . -eval "solve:do2(),erlang:halt()" | grep north
+do2() ->
+    L = parse_file("input1.txt"),
+    F = fun(Room) ->
+        io:format("~p ~s~n", [Room, decrypt(Room)])
+    end,
+    lists:foreach(F, L).
+
 test() ->
-    {"aaaaabbbzyx", 123, "abxyz"} = parse("aaaaa-bbb-z-y-x-123[abxyz]"),
-    {"abcdefgh", 987, "abcde"} = parse("a-b-c-d-e-f-g-h-987[abcde]"),
+    {"aaaaa-bbb-z-y-x", 123, "abxyz"} = parse("aaaaa-bbb-z-y-x-123[abxyz]"),
+    {"a-b-c-d-e-f-g-h", 987, "abcde"} = parse("a-b-c-d-e-f-g-h-987[abcde]"),
     
     [1,2,3] = remove_dups([1,2,3]),
     [3,2,1] = remove_dups([3,3,2,1,1]),
@@ -72,6 +90,10 @@ test() ->
     true = check_checksum(parse("a-b-c-d-e-f-g-h-987[abcde]")),
     true = check_checksum(parse("not-a-real-room-404[oarel]")),
     false = check_checksum(parse("totally-real-room-200[decoy]")),
+
+    278221 = do1(),
+
+    "very encrypted name" = decrypt(parse("qzmt-zixmtkozy-ivhz-343[zimth]")),
 
     ok.
 
