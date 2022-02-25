15b769264d349381461416c7d39b0d6830a2d9d3
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2022-02-25 19:02:34 +0100

    Stage 2 and some clean up

--- 2016/day6/solve.erl
+++ 2016/day6/solve.erl
@@ -1,36 +1,39 @@
 -module(solve).
 -compile(export_all).
 
-
 do1(Fname) ->
+    doit(Fname, fun(X, Y) -> X > Y end, 0).
+do2(Fname) ->
+    doit(Fname, fun(X, Y) -> X < Y end, 99999999).
+
+doit(Fname, Op, Init) ->
     {ok, FD} = file:open(Fname, [read]),
-    Maps = do1(FD, []),
-    F = fun(Key, Value, {MaxKey, MaxVal}) -> 
-            case Value > MaxVal of
+    Maps = read_and_count(FD, []),
+    MinMax = fun(Key, Value, {MaxKey, MaxVal}) -> 
+            case Op(Value, MaxVal) of
                 true ->
                     {Key,Value};
                 false ->
                     {MaxKey, MaxVal}
              end
          end,
-    lists:flatten(lists:map(fun({C, _}) -> C end, lists:map(fun(M) -> maps:fold(F, {nothing, 0}, M) end, Maps))).
+    R = lists:map(fun(M) -> maps:fold(MinMax, {nothing, Init}, M) end, Maps),
+    lists:flatten(lists:map(fun({C, _}) -> C end, R)).
+
+read_and_count(FD, Maps) ->
+    case file:read_line(FD) of
+        {ok, L} ->
+            Line = re:split(string:strip(L, right, $\n), "", [trim, {return, list}]),
+            Count = fun(Map, Char) ->
+                    maps:update_with(Char, fun(X) -> X+1 end, 1, Map)
+                end,
+            read_and_count(FD, lists:zipwith(Count, init_maps(Maps, length(Line)), Line));
+        eof ->
+            Maps
+    end.
 
 init_maps([], N) ->
     lists:map(fun(_) -> maps:new() end, lists:seq(1, N));
 init_maps(M, _) ->
     M.
-
-do1(FD, Maps) ->
-    case file:read_line(FD) of
-        {ok, L} ->
-            Line = re:split(string:strip(L, right, $\n), "", [trim, {return, list}]),
-            F = fun(Map, Char) ->
-                    maps:update_with(Char, fun(X) -> X+1 end, 1, Map)
-                end,
-            do1(FD, lists:zipwith(F, init_maps(Maps, length(Line)), Line));
-        eof ->
-            Maps
-    end.
-
-
 
