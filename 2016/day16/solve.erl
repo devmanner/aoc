4e1dbe46c5cd440f8e77bf84e67881f35e912ed9
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2022-09-19 15:53:19 +0200

    Fixed so that level2 works

--- 2016/day16/solve.erl
+++ 2016/day16/solve.erl
@@ -1,25 +1,26 @@
 -module(solve).
 -export([test/0]).
 -export([do1/0, do2/0]).
+-export([fill/2]).
 
 -define(INIT_STATE, [0,0,1,1,1,1,0,1,1,1,1,1,0,1,0,0,0]).
 
-next(A) ->
-    A ++ [0] ++ lists:map(fun(X) -> X bxor 1 end, lists:reverse(A)).
-
 checksum(X) ->
     C = checksum(X, []),
-    case length(C) rem 2 of
+    case length(C) band 1 of
         1 -> C;
         0 -> checksum(C)
     end.
 
 checksum([] , Acc) ->
-    Acc;
+    lists:reverse(Acc);
 checksum([X, Y|T], Acc) ->
     % We are setting it to 1 if X and Y are the same
     % bnot 0 is not 1, (0xfffffe) that is why we band it with 1
     checksum(T, [(bnot (X bxor Y)) band 1|Acc]).
+
+next(A) ->
+    lists:concat([A, [0], lists:reverse(lists:map(fun(X) -> X bxor 1 end, A))]).
 
 fill(List, Length) ->
     case length(List) < Length of
@@ -60,6 +61,6 @@ test() ->
 
     [1,0,0,1,1,0,1,0,0,1,0,0,1,0,0,1,0] = do1(),
 
-    do2(),
+    [1,0,1,0,1,0,1,1,1,1,0,1,0,0,0,1,1] = do2(),
 
     ok.
