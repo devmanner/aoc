a512cae1df2ea0b4003c9d63c18243a133d65ce0
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2022-09-23 14:01:31 +0200

    Cheanup and optimization

--- 2016/day17/solve.erl
+++ 2016/day17/solve.erl
@@ -4,41 +4,26 @@
 
 -define(PASSCODE, "njfxhljp").
 
-is_open(C) ->
-    case string:to_integer([C]) of
-        {error, no_integer} ->
-            case C of
-                $a -> closed;
-                _ -> open
-            end;
-        {X, []} when is_integer(X) -> closed
-    end.
+is_open(D) when D =< 10 -> closed;
+is_open(_D) -> open.
 
-hash(Path) ->
-    hash(?PASSCODE, Path).
-hash(Passcode, Path) ->
-    <<X1, X2, _/binary>> = crypto:hash(md5, Passcode ++ Path),
-    string:lowercase(binary:bin_to_list(binary:encode_hex(<<X1,X2>>))).
+% This would in a C-like language be:
+% Pred ? X : Y
+if_else(true, X, _Y) -> X;
+if_else(false, _X, Y) -> Y. 
 
-door_state(Passcode, Row, Col, Path) ->
-    [U, D, L, R] = lists:map(fun(X) -> is_open(X) end, hash(Passcode, Path)),
-    Up = case Row == 0 of
-        true -> closed;
-        false -> U
-    end,
-    Down = case Row == 3 of
-        true -> closed;
-        false -> D
-    end,
-    Left = case Col == 0 of
-        true -> closed;
-        false -> L
-    end,
-    Right = case Col == 3 of
-        true -> closed;
-        false -> R
-    end,
-    [Up, Down, Left, Right].
+door_states(Passcode, Path) ->
+    <<U:4,D:4,L:4,R:4, _/binary>> = crypto:hash(md5, Passcode ++ Path),
+    [is_open(U), is_open(D), is_open(L), is_open(R)].
+
+door_states(Passcode, Row, Col, Path) ->
+    [U, D, L, R] = door_states(Passcode, Path),
+    [
+        if_else(Row == 0, closed, U),
+        if_else(Row == 3, closed, D),
+        if_else(Col == 0, closed, L),
+        if_else(Col == 3, closed, R)
+    ].
 
 shortest_path(Passcode) ->
     path(fun(X, Y) -> X < Y end, Passcode, 0, 0, []).
@@ -49,19 +34,15 @@ longest_path(Passcode) ->
 path(_Op, _Passcode, 3, 3, Path) ->
     Path;
 path(Op, Passcode, Row, Col, Path) ->
-    case length(Path) > 999 of
-        true -> dead_end;
-        false -> 
-            [Up, Down, Left, Right] = door_state(Passcode, Row, Col, Path),
-            case list(Op, [
-                        path(Op, Passcode, Right, Row, Col+1, Path ++ [$R]),
-                        path(Op, Passcode, Down, Row+1, Col, Path ++ [$D]),
-                        path(Op, Passcode, Left, Row, Col-1, Path ++ [$L]),
-                        path(Op, Passcode, Up, Row-1, Col, Path ++ [$U])
-                    ]) of
-                [] -> dead_end;
-                L -> L
-            end
+    [Up, Down, Left, Right] = door_states(Passcode, Row, Col, Path),
+    case list(Op, [
+                path(Op, Passcode, Right, Row, Col+1, Path ++ [$R]),
+                path(Op, Passcode, Down, Row+1, Col, Path ++ [$D]),
+                path(Op, Passcode, Left, Row, Col-1, Path ++ [$L]),
+                path(Op, Passcode, Up, Row-1, Col, Path ++ [$U])
+            ]) of
+        [] -> dead_end;
+        L -> L
     end.
 
 path(_Op, _Passcode, closed, _Row, _Col, _Path) ->
@@ -75,12 +56,12 @@ list(Op, L) ->
         [H|T] -> list(Op, T, H)
     end.
 
-list(_Op, [], Shortest) ->
-    Shortest;
-list(Op, [L|T], Shortest) ->
-    case Op(length(L), length(Shortest)) of
+list(_Op, [], Acc) ->
+    Acc;
+list(Op, [L|T], Acc) ->
+    case Op(length(L), length(Acc)) of
         true -> list(Op, T, L);
-        false -> list(Op, T, Shortest)
+        false -> list(Op, T, Acc)
     end.
 
 do1() ->
@@ -90,13 +71,20 @@ do2() ->
     length(longest_path(?PASSCODE)).
 
 test() ->
-    closed = is_open($1),
-    open = is_open($A),
+    closed = is_open(1),
+    closed = is_open(10), % 0xa
+    open = is_open(11), % 0xb
 
-    "ced9" = hash("hijkl", []),
-    "f2bc" = hash("hijkl", [$D]),
-    "5745" = hash("hijkl", [$D, $R]),
-    "528e" = hash("hijkl", [$D, $U]),
+    [open, open, open, closed] = door_states("hijkl", []),
+    [open, closed, open, open] = door_states("hijkl", [$D]),
+    [closed, closed, closed, closed] = door_states("hijkl", [$D, $R]),
+    [closed, closed, closed, open] = door_states("hijkl", [$D, $U]),
+
+    % "3" + empty path is a passphrase that implies 
+    [closed, open, open, open] = door_states("3", 0, 2, []),
+    [open, closed, open, open] = door_states("3", 3, 2, []),
+    [open, open, closed, open] = door_states("3", 2, 0, []),
+    [open, open, open, closed] = door_states("3", 2, 3, []),
 
     Shortest = fun(X, Y) -> X < Y end,
     [] = list(Shortest, [dead_end, dead_end]),
@@ -117,6 +105,6 @@ test() ->
     830 = length(longest_path("ulqzkmiv")),
 
     650 = do2(),
-    
+
     ok.
 
