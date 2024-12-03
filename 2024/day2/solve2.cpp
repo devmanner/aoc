7f92bea767c976dfde6ed502b13ddfd04b0230e4
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2024-12-03 20:35:16 +0100

    Working now

--- 2024/day2/solve2.cpp
+++ 2024/day2/solve2.cpp
@@ -11,29 +11,40 @@ using namespace std;
 template <typename Iterator>
 bool is_safe(Iterator fst, Iterator lst) {
     typename iterator_traits<Iterator>::value_type prev = *fst;
-    int incdec = 0;
-    int errors = 0;
+    int incdec;
+    ++fst;
 
+    if (prev < *fst && *fst-prev <= 3)
+        incdec = 1;
+    else if (prev > *fst && prev-*fst <= 3)
+        incdec = -1;
+    else
+        return false;
 
-    for (++fst; fst != lst; ++fst) {
-        if (*fst == prev && incdec == 0)
-            errors++;
-        else {
-            if (incdec == 0) {
-                incdec = 1;
-                if (*fst < prev)
-                    incdec = -1;
-            }
-            typename iterator_traits<Iterator>::value_type diff = ((*fst - prev) * incdec);
-            if (diff > 3 || diff < 1)
-                errors++;
-        }
+    for (; fst != lst; ++fst) {
+        typename iterator_traits<Iterator>::value_type diff = ((*fst - prev) * incdec);
+
+        if (diff > 3 || diff < 1)
+            return false;
         prev = *fst;
     }
-    
-    return errors <= 1;
+    return true;
 }
 
+template <typename Iterator>
+bool is_safe2(Iterator fst, Iterator lst) {
+    if (is_safe(fst, lst))
+        return true;
+
+    for (Iterator to_del = fst; to_del != lst; ++to_del) {        
+        vector<typename iterator_traits<Iterator>::value_type> tmp;
+        copy(fst, to_del, back_inserter(tmp));
+        copy(to_del+1, lst, back_inserter(tmp));
+        if (is_safe(tmp.begin(), tmp.end()))
+            return true;
+    }
+    return false;
+}
 
 int main() {
     string line;
@@ -46,9 +57,11 @@ int main() {
 
         cout << "report: ";
         copy(report.begin(), report.end(), ostream_iterator<int>(cout, " "));
-        cout << " is safe? " << is_safe(report.begin(), report.end()) << endl;
+        
+        bool s = is_safe2(report.begin(), report.end());
+        cout << " is safe? " << s << endl;
 
-        safe += is_safe(report.begin(), report.end());// ? safe + 1 : safe;
+        safe += s;
     }
 
     cout << safe << endl;
