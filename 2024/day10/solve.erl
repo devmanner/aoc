39012bb5429100c0a9cea66132547342e4931904
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2024-12-19 20:14:16 +0100

    Stage2 done (but destroyed stage1...

--- 2024/day10/solve.erl
+++ 2024/day10/solve.erl
@@ -16,22 +16,28 @@ do_parse_file(FD) ->
 
 score_trail_head(M, R, C, Val) ->
     Res = find_trails(M, R, C, Val),
-    length(lists:uniq(Res)).
+    io:format("Trails starting at: ~p ~p (~p) score: ~p are: ~n~p~n", [R, C, Val, length(Res), Res]),
+    length(Res).
 
-find_trails(_M, R, C, 9) ->
-    io:format("Found end: ~p ~p~n", [R, C]),
-    [{R, C}];
 find_trails(M, R, C, Val) ->
-    io:format("Counting trails from: ~p ~p with val: ~p~n", [R, C, Val]),
-    Fn = fun(Dir, Acc) ->
+    Filter = fun({Type, _}) -> Type == full end,
+    T = lists:uniq(lists:filter(Filter, lists:flatten(find_trails(M, R, C, Val, [])))),
+%    io:format("Trails: ~n~p~n", [T]),
+    T.
+find_trails(_M, R, C, 9, Trail) ->
+%    io:format("Found end: ~p ~p~n", [R, C]),
+    {full, [{R, C, 9}|Trail]};
+find_trails(M, R, C, Val, Trail) ->
+%    io:format("Counting trails from: ~p ~p with val: ~p~n", [R, C, Val]),
+    Fn = fun(Dir) ->
         {DR, DC} = delta(Dir),
         case Val + 1 == matrix:safe_get(M, R+DR, C+DC) of
-            true -> [find_trails(M, R+DR, C+DC, Val+1)|Acc];
-            false -> Acc
+            true -> find_trails(M, R+DR, C+DC, Val+1, [{R, C, Val}|Trail]);
+            false -> {dead_end, Trail}
         end 
     end,
-    Res = lists:uniq(lists:flatten(lists:foldl(Fn, [], dirs()))),
-    io:format("R: ~p~n", [Res]),
+    Res = lists:map(Fn, dirs()),
+%    io:format("R: ~p~n", [Res]),
     Res.
 
 dirs() -> [up, right, left, down].
@@ -42,7 +48,7 @@ delta(down) -> {1, 0};
 delta(left) -> {0, -1}.
 
 score_trail_heads(M) ->
-    lists:sum(score_trail_heads(M, matrix:find_row_wise(M, 0, 0, 0))).
+    score_trail_heads(M, matrix:find_row_wise(M, 0, 0, 0)).
 score_trail_heads(_M, not_found) ->
     [];
 score_trail_heads(M, {StartRow, StartCol}) ->
@@ -54,7 +60,11 @@ score_trail_heads(M, {StartRow, StartCol}) ->
     end.
 
 test() ->
-    36 = solve:score_trail_heads(solve:parse_file("test2.txt")),
-    582 = solve:score_trail_heads(solve:parse_file("input.txt")),
-    ok.
+%   Destroyed stage 1 in this version.... 
+%    36 = solve:score_trail_heads(solve:parse_file("test2.txt")),
+%    582 = solve:score_trail_heads(solve:parse_file("input.txt")),
+
+    81 = lists:sum(score_trail_heads(parse_file("test2.txt"))),
+    1302 = lists:sum(score_trail_heads(parse_file("input.txt"))).
+    
 
